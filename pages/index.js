1df59f37f4310f9310548cd69a2ff4867a44dbad
Emil <emilijus.simanski@inntec.lt>
2020-03-25 10:41:39 +0100

    add form

--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,142 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { Form, Field } from 'react-final-form';
+
+const onSubmit = values => {
+	window.alert(JSON.stringify(values, 0, 2));
+};
 
 const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+	<div className='container'>
+		<Head>
+			<title>Create Next App</title>
+			<link rel='icon' href='/favicon.ico' />
+		</Head>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+		<main>
+			<Form
+				className='final-form'
+        onSubmit={onSubmit}
+        initialValues={{ firstCheck: true, color: '#000000' }}
+				// validate={validate}
+				render={({ handleSubmit, values }) => (
+					<form onSubmit={handleSubmit}>
+						<h2>Final form example</h2>
+						<div className='section'>
+							<div>
+								<label>First Name</label>
+								<Field
+									name='firstName'
+									component='input'
+									placeholder='First Name'
+								/>
+							</div>
+							<div>
+								<label>Last Name</label>
+								<Field
+									name='lastName'
+									component='input'
+									placeholder='First Name'
+								/>
+							</div>
+						</div>
+						<div className='section'>
+							<Field name='firstCheck' component='input' type='checkbox' checked={values.firstCheck} />
+							<label>First</label>
+							<Field name='secondCheck' component='input' type='checkbox' />
+							<label>Second</label>
+							<Field name='thirdCheck' component='input' type='checkbox' />
+							<label>Third</label>
+						</div>
+						<div className='section'>
+							<div>
+								<label>Choose color: </label>
+								<Field
+									name='color'
+									component='input'
+									type='color'
+									value='#000000'
+								/>
+							</div>
+						</div>
+						<div className='section'>
+							<div>
+								<label for='datemax'>Enter a date before 2020-03-24:</label>
+								<Field
+									name='maxDate'
+									component='input'
+									type='date'
+									min='2020-03-24'
+								/>
+							</div>
+						</div>
+						<button className='submit' type='submit'>
+							Submit
+						</button>
+						<pre>{JSON.stringify(values, 0, 2)}</pre>
+					</form>
+				)}
+			/>
+		</main>
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+		<footer></footer>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+		<style jsx>{``}</style>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+		<style jsx global>{`
+			html,
+			body {
+				padding: 0;
+				margin: 0;
+				font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+					Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+			}
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+			* {
+				box-sizing: border-box;
+			}
 
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+			main {
+        position: relative;
+				// margin: 5% 25%;
       }
 
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
+      @media screen and (min-width: 750px) {
+        form {
+          position: absolute;
+          left: 40%;
+          top: 50%;
         }
       }
-    `}</style>
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      @media screen and (max-width: 750px) {
+        form {
+          position: absolute;
+          left: 20%;
+          top: 50%;
+        }
       }
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
 
-export default Home
+			.section {
+        width: 100%;
+				margin-bottom: 0.5rem;
+			}
+
+			.section > div > label {
+				margin-bottom: 0.5rem;
+				display: block;
+			}
+
+			.section > div > input {
+				margin-bottom: 0.5rem;
+				display: block;
+			}
+
+			.submit {
+				margin-top: 0.5rem;
+			}
+		`}</style>
+	</div>
+);
+
+export default Home;
